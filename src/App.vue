91cd51b0b189a72d4effbdbc35922da49120cc20
Francesca Barletta <wallebar@gmail.com>
2024-04-09 20:09:36 +0200

    add appHero

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <script>
  import AppTitle from './components/AppTitle.vue';
+ import AppHero from './components/AppHero.vue';
  export default {
     components:{
-      AppTitle
+      AppTitle,
+      AppHero
     }
  }
 </script>
 
 <template>
   <AppTitle/>
+  <AppHero/>
 </template>
+<style>
+div {
+ margin: 0 auto;
+ width: 900px;
+}
+img {
+  width: 100%;
+}
+</style>
